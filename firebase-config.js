e8cc99d5411bb8e6178e1f33123aeb331a79a38c
lapardo <pardolaura@gmail.com>
2026-02-12 17:29:01 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,14 @@
 // You'll need to replace these with your actual Firebase project credentials
 // Instructions for getting these values are in the setup guide
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  databaseURL: "YOUR_DATABASE_URL"
+const firebaseConfig = {	
+  apiKey: "AIzaSyBgmOpJ_gllY3mffcZrx6tNJGyStAEq_3c",
+  authDomain: "portfolio-path-gallery.firebaseapp.com",
+  databaseURL: "https://portfolio-path-gallery-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "portfolio-path-gallery",
+  storageBucket: "portfolio-path-gallery.firebasestorage.app",
+  messagingSenderId: "862527857380",
+  appId: "1:862527857380:web:543e55ca69d471235542f1"	
 };
 
 export default firebaseConfig;
